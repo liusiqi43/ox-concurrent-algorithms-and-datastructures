018a7d34c231ea83bf2d348a1c926b63e2180431
Siqi Liu <me@siqi.fr>
2015-11-18 10:52:02 +0000

    updated documentation

--- 3/LFLinkedArrayList.scala
+++ 3/LFLinkedArrayList.scala
@@ -2,10 +2,21 @@ import scala.reflect.ClassTag
 import java.util.concurrent.locks.ReentrantLock
 import java.util.concurrent.atomic.{AtomicInteger, AtomicReference, AtomicReferenceArray}
 
+/**
+ * Lock-free linked arrary list based queue is implemented with the following
+ * ideas:
+ * - enqueueIndex points to the next slot to insert or equals to size.
+ * - dequeueIndex points to the next element to dequeue.
+ * - enqueue operation guarantees that when its call returns, enqueueIndex is
+ *   updated until enqueueIndex points to null or equals to size.
+ * - dequeue operation use CAS operation to first claim a dequeueIndex, then 
+ *   return the corresponding element.
+ */
+
 class LFLinkedArrayList [T: ClassTag] extends ox.cads.collection.Queue[T] {
   private class Node{
-    val head = new AtomicInteger(0)
-    val tail = new AtomicInteger(0)
+    val enqueueIndex = new AtomicInteger(0)
+    val dequeueIndex = new AtomicInteger(0)
 
     val data = new AtomicReferenceArray[T](size)
     var next = new AtomicReference[Node](null)
@@ -21,32 +32,49 @@ class LFLinkedArrayList [T: ClassTag] extends ox.cads.collection.Queue[T] {
     var done = false
     while (!done) {
       var tailNode = tail.get
-      var tailIndex = tailNode.tail.get
-      if (tailIndex < size) {
+      var enqueueIndex = tailNode.enqueueIndex.get
+      if (enqueueIndex < size) {
         // Looks like there is an empty slot.
-        while (!done && tailIndex < size) {
-          // try to set value to the slot.
-          if (tailNode.data.compareAndSet(tailIndex, null.asInstanceOf[T], x)) {
+        while (!done && enqueueIndex < size) {
+          // try to set value to the slot if null is at enqueueIndex.
+          if (tailNode.data.compareAndSet(enqueueIndex, null.asInstanceOf[T], x))
             done = true
+          // Invariant: enqueueIndex <= size.
+          // Either the successful thread updates the enqueueIndex itself until 
+          // enqueueIndex points to null or mark the node as full by setting it
+          // to size or the thread failed to enqueue its element updates the 
+          // enqueueIndex for the sucessful thread (which could be descheduled).
+          //
+          // This guarantees that when a call to enqueue returns, the element 
+          // inserted could be retrieved by subsequent dequeue operations as 
+          // both the element itself and the enqueueIndex are updated.
+          //
+          // In addition, the operation will proceed correctly if a thread is
+          // descheduled after enqueuing its element as other enqueuing threads
+          // would update enqueueIndex for it.
+          while (enqueueIndex < size && tailNode.data.get(enqueueIndex) != null) {
+            tailNode.enqueueIndex.compareAndSet(enqueueIndex, enqueueIndex+1)
+            enqueueIndex = tailNode.enqueueIndex.get
           }
-          // tailIndex == size --> node is full
-          // Invariant: tailIndex <= size
-        while (tailIndex < size && tailNode.data.get(tailIndex) != null) {
-          tailNode.tail.compareAndSet(tailIndex, tailIndex+1)
-          tailIndex = tailNode.tail.get
-        }
-
         }
       } else {
         // tail node is full.
-        // create new node.
+        // create new node locally to the thread and compete to set the next 
+        // field of the tail node to this new node.
         val node = new Node
-        node.data.set(node.tail.getAndIncrement, x)
+        node.data.set(node.enqueueIndex.getAndIncrement, x)
 
+        // The only two places where we update tail is either in the successful 
+        // thread or in the failed thread. Successful thread attempts to update 
+        // tail: 1) either other threads don't intervene, in which case the CAS
+        // on tail finishes successfully 2) other threads intervened and updated
+        // tail field for the successful thread. 
+        // This allows successful thread to be descheduled after the CAS on 
+        // tailNode.next field. Hence descheduling of one thread won't block
+        // other threads.
         if (tailNode.next.compareAndSet(null, node)) {
           done = true
           tail.compareAndSet(tailNode, node)
-          // println("New Node " + x)
         } else {
           var tailNode = tail.get
           var next = tailNode.next.get
@@ -67,25 +95,33 @@ class LFLinkedArrayList [T: ClassTag] extends ox.cads.collection.Queue[T] {
     while (true) {
       val headNode = head.get
       val next = headNode.next.get
-      val headIndex = headNode.head.get
-      var tailIndex = headNode.tail.get
+      val dequeueIndex = headNode.dequeueIndex.get
+      var enqueueIndex = headNode.enqueueIndex.get
 
-      if (headIndex == tailIndex && next == null) {
+      // enqueue operation guarantees that enqueueIndex is updated to 
+      // point to null after its return. Hence, if enqueue operation hasn't
+      // finished updating enqueueIndex (descheduled after enqueuing the 
+      // element) returning None would still be linearizable as the 
+      // enqueue/dequeue operations are concurrent.
+      // Otherwise, we have the guarantee that the enqueueIndex is up-to-date,
+      // hence the following condition guarantees that there is nothing to 
+      // return.
+      if (dequeueIndex == enqueueIndex && next == null) {
         return None
       }
       // head node is not empty or head node is empty but head.next != null.
-      assert(tailIndex <= size)
-      if (headIndex < tailIndex) {
-        if (headNode.head.compareAndSet(headIndex, headIndex+1))
-          return Some(headNode.data.get(headIndex))
-      } else if (headIndex == tailIndex && headIndex == size) {
+      assert(enqueueIndex <= size)
+      if (dequeueIndex < enqueueIndex) {
+        // Compete to claim the dequeueIndex first, then return the 
+        // corresponding element.
+        if (headNode.dequeueIndex.compareAndSet(dequeueIndex, dequeueIndex+1))
+          return Some(headNode.data.get(dequeueIndex))
+      } else if (dequeueIndex == enqueueIndex && dequeueIndex == size) {
         // This node is used up, move to next. 
         // If it succeed, return to retry, if not, some other threads
         // have updated head so we retry as well.
         head.compareAndSet(headNode, next)
-      } else {
-        assert(false)
-      }
+      } 
     }
     assert(false)
     None
--- 3/report.scala
+++ 3/report.scala
@@ -66,15 +66,25 @@ class LinkedArrayList [T: ClassTag] extends ox.cads.collection.Queue[T] {
   }
 }
 
-// Lock-free LinkedArrayList
 import scala.reflect.ClassTag
 import java.util.concurrent.locks.ReentrantLock
 import java.util.concurrent.atomic.{AtomicInteger, AtomicReference, AtomicReferenceArray}
 
+/**
+ * Lock-free linked arrary list based queue is implemented with the following
+ * ideas:
+ * - enqueueIndex points to the next slot to insert or equals to size.
+ * - dequeueIndex points to the next element to dequeue.
+ * - enqueue operation guarantees that when its call returns, enqueueIndex is
+ *   updated until enqueueIndex points to null or equals to size.
+ * - dequeue operation use CAS operation to first claim a dequeueIndex, then 
+ *   return the corresponding element.
+ */
+
 class LFLinkedArrayList [T: ClassTag] extends ox.cads.collection.Queue[T] {
   private class Node{
-    val head = new AtomicInteger(0)
-    val tail = new AtomicInteger(0)
+    val enqueueIndex = new AtomicInteger(0)
+    val dequeueIndex = new AtomicInteger(0)
 
     val data = new AtomicReferenceArray[T](size)
     var next = new AtomicReference[Node](null)
@@ -90,25 +100,46 @@ class LFLinkedArrayList [T: ClassTag] extends ox.cads.collection.Queue[T] {
     var done = false
     while (!done) {
       var tailNode = tail.get
-      var tailIndex = tailNode.tail.get
-      if (tailIndex < size) {
+      var enqueueIndex = tailNode.enqueueIndex.get
+      if (enqueueIndex < size) {
         // Looks like there is an empty slot.
-        while (!done && tailIndex < size) {
-          // try to set value to the slot.
-          if (tailNode.data.compareAndSet(tailIndex, null.asInstanceOf[T], x)) {
+        while (!done && enqueueIndex < size) {
+          // try to set value to the slot if null is at enqueueIndex.
+          if (tailNode.data.compareAndSet(enqueueIndex, null.asInstanceOf[T], x))
             done = true
+          // Invariant: enqueueIndex <= size.
+          // Either the successful thread updates the enqueueIndex itself until 
+          // enqueueIndex points to null or mark the node as full by setting it
+          // to size or the thread failed to enqueue its element updates the 
+          // enqueueIndex for the sucessful thread (which could be descheduled).
+          //
+          // This guarantees that when a call to enqueue returns, the element 
+          // inserted could be retrieved by subsequent dequeue operations as 
+          // both the element itself and the enqueueIndex are updated.
+          //
+          // In addition, the operation will proceed correctly if a thread is
+          // descheduled after enqueuing its element as other enqueuing threads
+          // would update enqueueIndex for it.
+          while (enqueueIndex < size && tailNode.data.get(enqueueIndex) != null) {
+            tailNode.enqueueIndex.compareAndSet(enqueueIndex, enqueueIndex+1)
+            enqueueIndex = tailNode.enqueueIndex.get
           }
-          // tailIndex == size --> node is full
-          // Invariant: tailIndex <= size
-          tailNode.tail.compareAndSet(tailIndex, tailIndex+1)
-          tailIndex = tailNode.tail.get
         }
       } else {
         // tail node is full.
-        // create new node.
+        // create new node locally to the thread and compete to set the next 
+        // field of the tail node to this new node.
         val node = new Node
-        node.data.set(node.tail.getAndIncrement, x)
+        node.data.set(node.enqueueIndex.getAndIncrement, x)
 
+        // The only two places where we update tail is either in the successful 
+        // thread or in the failed thread. Successful thread attempts to update 
+        // tail: 1) either other threads don't intervene, in which case the CAS
+        // on tail finishes successfully 2) other threads intervened and updated
+        // tail field for the successful thread. 
+        // This allows successful thread to be descheduled after the CAS on 
+        // tailNode.next field. Hence descheduling of one thread won't block
+        // other threads.
         if (tailNode.next.compareAndSet(null, node)) {
           done = true
           tail.compareAndSet(tailNode, node)
@@ -132,38 +163,58 @@ class LFLinkedArrayList [T: ClassTag] extends ox.cads.collection.Queue[T] {
     while (true) {
       val headNode = head.get
       val next = headNode.next.get
-      val headIndex = headNode.head.get
-      val tailIndex = headNode.tail.get
-      if (headIndex == tailIndex && next == null)
-        return None
+      val dequeueIndex = headNode.dequeueIndex.get
+      var enqueueIndex = headNode.enqueueIndex.get
 
+      // enqueue operation guarantees that enqueueIndex is updated to 
+      // point to null after its return. Hence, if enqueue operation hasn't
+      // finished updating enqueueIndex (descheduled after enqueuing the 
+      // element) returning None would still be linearizable as the 
+      // enqueue/dequeue operations are concurrent.
+      // Otherwise, we have the guarantee that the enqueueIndex is up-to-date,
+      // hence the following condition guarantees that there is nothing to 
+      // return.
+      if (dequeueIndex == enqueueIndex && next == null) {
+        return None
+      }
       // head node is not empty or head node is empty but head.next != null.
-      assert(tailIndex <= size)
-      if (headIndex < tailIndex) {
-        if (headNode.head.compareAndSet(headIndex, headIndex+1))
-          return Some(headNode.data.get(headIndex))
-      } else if (headIndex == tailIndex && headIndex == size) {
+      assert(enqueueIndex <= size)
+      if (dequeueIndex < enqueueIndex) {
+        // Compete to claim the dequeueIndex first, then return the 
+        // corresponding element.
+        if (headNode.dequeueIndex.compareAndSet(dequeueIndex, dequeueIndex+1))
+          return Some(headNode.data.get(dequeueIndex))
+      } else if (dequeueIndex == enqueueIndex && dequeueIndex == size) {
         // This node is used up, move to next. 
         // If it succeed, return to retry, if not, some other threads
         // have updated head so we retry as well.
         head.compareAndSet(headNode, next)
-      } else {
-        assert(false)
-      }
+      } 
     }
     assert(false)
     None
   }
 }  //  class LFLinkedArrayList
 
-// Linearizability test: PASSED.
-// For 10k repetitions (4 threads):
+
+// Linearizability test PASSED for lock-based and lock-free
+// queue passed for:
+// 
+// 1k repetitions (4 threads):
+// [Lock-based queue] time taken: 2041.095ms
+// [Lock-free queue] time taken: 875.582ms
+//
+// 10k repetitions (4 threads):
 // [Lock-based queue] time taken: 9456.371ms
 // [Lock-free queue] time taken: 6820.488ms
 // 
-// For 1k repetitions (4 threads):
-// [Lock-based queue] time taken: 1999.154ms
-// [Lock-free queue] time taken: 772.846ms
+// 100k repetitions (4 threads):
+// [Lock-based queue] time taken: 58717.343ms
+// [Lock-free queue] time taken: 56612.762ms
+//
+// 1 million repetitions (4 threads):
+// [Lock-based queue] time taken: 573555.899ms
+// [Lock-free queue] time taken: 568426.355ms
 import scala.collection.immutable.Queue
 import ox.cads.util.ThreadUtil
 import ox.cads.testing.{LinTester, DFSLinearizabilityTester}
@@ -185,7 +236,7 @@ object Test {
     val random = new scala.util.Random
     for (i <- 0 until 200) {
       if(random.nextFloat <= 0.3){
-        val x = random.nextInt(1000)
+        val x = random.nextInt(100000)
         tester.log(me, _.enqueue(x), "enqueue("+x+")", seqEnqueue(x))
       }
       else tester.log(me, _.dequeue, "dequeue", seqDequeue)
@@ -193,8 +244,10 @@ object Test {
   }
 
   def main(args: Array[String]) {
+    val reps = args(0).toInt
+
     val t0 = System.nanoTime
-    for (i <- 0 until 10000) {
+    for (i <- 0 until reps) {
       val concQueue = new LC
       val seqQueue = Queue[Int]()
       val tester = new DFSLinearizabilityTester(seqQueue, concQueue, 4, worker _, 800)
@@ -203,7 +256,7 @@ object Test {
     println("[Lock-based queue] time taken: " + (System.nanoTime - t0)/1e6 + "ms")
 
     val t1 = System.nanoTime
-    for (i <- 0 until 10000) {
+    for (i <- 0 until reps) {
       val concQueue = new LFC
       val seqQueue = Queue[Int]()
       val tester = new DFSLinearizabilityTester(seqQueue, concQueue, 4, worker _, 800)
